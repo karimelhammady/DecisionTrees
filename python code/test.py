7f5d2234c4d40cd713e308dca3419df0187fda00
karimelhammady <31138502+karimelhammady@users.noreply.github.com>
2020-06-11 21:17:53 +0200

    Update test.py

--- python code/test.py
+++ python code/test.py
@@ -6,7 +6,7 @@ from scipy import stats
 
 np.set_printoptions(threshold=sys.maxsize)
 # read dataset file
-df = pd.read_csv('D:/ASU/data mining/final research/code/dataset_train.csv')
+df = pd.read_csv('D:/ASU/data mining/final research/dataset/dataset_train.csv')
 #  take part of the data to be used for plotting
 part = df.iloc[:, 4]
 # display quick summary for data
@@ -14,10 +14,13 @@ df.info()
 # plot all attributes in one graph
 df.plot()
 # plot a certain range
+
+plt.show()
+part.plot.area(figsize=(9, 4), subplots=True)
+plt.show()
+pd.DataFrame.hist(df)
+plt.show()
 part.plot.box()
-axs = part.plot.area(figsize=(9, 4), subplots=True)
-pd.DataFrame.hist(part)
-part.boxplot()
 plt.show()
 
 # perform stats on all attrbiutes
@@ -28,10 +31,8 @@ for i in df:
     z_scores = stats.zscore(df[i])
     abs_z_scores = np.abs(z_scores)
     new_df = df[abs_z_scores <= 0.5]
-    
+
 # plot after removing outliers
 part = new_df.iloc[:, 4]
 part.plot.box()
 plt.show()
-
-
